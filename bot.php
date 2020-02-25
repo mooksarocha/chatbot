83e7c235c51e7aa68f6f9f3c8b5ef003d6c0563b
mooksarocha <61469581+mooksarocha@users.noreply.github.com>
2020-02-26 01:57:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
             $url = 'https://api.line.me/v2/bot/message/reply';
             $data = [
                 'replyToken' => $replyToken,
-                'messages' => ['ว่าไงค้าบ']
+                'messages' => [$messages]
             ];
             $post = json_encode($data);
             $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
