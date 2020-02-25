7959ea1b0dd785cd56b2eb84425b9b4d72d6f5cf
mooksarocha <61469581+mooksarocha@users.noreply.github.com>
2020-02-26 02:08:10 +0700

    Create flex.php

---
+++ flex.php
@@ -0,0 +1,43 @@
+{
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "bubble",
+    "direction": "ltr",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "Emergency@PLK",
+          "align": "center"
+        }
+      ]
+    },
+    "hero": {
+      "type": "image",
+      "url": "https://developers.line.biz/assets/images/services/bot-designer-icon.png",
+      "align": "center",
+      "gravity": "center",
+      "size": "full",
+      "aspectRatio": "1.51:1",
+      "aspectMode": "fit",
+      "backgroundColor": "#F2C5C5"
+    },
+    "footer": {
+      "type": "box",
+      "layout": "horizontal",
+      "contents": [
+        {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "Button",
+            "uri": "https://linecorp.com"
+          }
+        }
+      ]
+    }
+  }
+}
